26afc4e4f0c97c29cf02508b061432269c17a0fa
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-11-10 18:22:44 -0500

    added loadOoptions

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,8 +1,27 @@
 import React, { useState } from "react";
 import { SearchDataProps } from "@/types/component-types";
 
-const Search: React.FC<SearchDataProps> = ({onSearchChange}) => {
+const Search: React.FC<SearchDataProps> = ({onSearchChange, }) => {
     const [search, setSearch] = useState(null);
+
+    const loadOptions = (inputValue: SearchDataProps) => {
+        return fetch(
+          `https://api.openweathermap.org/data/2.5/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+    
+        )
+          .then((response) => response.json())
+          .then((response) => {
+            return {
+              options: response.data.map((city: SearchDataProps) => {
+                return {
+                  value: `${city.latitude} ${city.longitude}`,
+                  label: `${city.name}, ${city.countryCode}`,
+                };
+              }),
+            };
+          });
+      };
+    
 
     return (
         <div>
--- src/types/component-types.ts
+++ src/types/component-types.ts
@@ -2,4 +2,11 @@ export interface SearchDataProps {
   value: string;
   label: string;
   onSearchChange: string;
+  inputValue: string;
+  city: string;
+  latitude: string;
+  longitude: string;
+  countryCode: string;
+  name: string;
+
 }
