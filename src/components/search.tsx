f341735478b321d44615b1abff63f5e94ba8b530
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-11-10 18:37:13 -0500

    added icons and paginate and handleChange

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,31 +1,45 @@
 import React, { useState } from "react";
 import { SearchDataProps } from "@/types/component-types";
+import { AsyncPaginate } from "react-select-async-paginate";
 
-const Search: React.FC<SearchDataProps> = ({onSearchChange, }) => {
-    const [search, setSearch] = useState(null);
+const Search: React.FC<SearchDataProps> = ({ onSearchChange }) => {
+  const [search, setSearch] = useState<null | SearchDataProps>(null);
 
-    const loadOptions = (inputValue: SearchDataProps) => {
-        return fetch(
-          `https://api.openweathermap.org/data/2.5/cities?minPopulation=1000000&namePrefix=${inputValue}`,
-    
-        )
-          .then((response) => response.json())
-          .then((response) => {
-            return {
-              options: response.data.map((city: SearchDataProps) => {
-                return {
-                  value: `${city.latitude} ${city.longitude}`,
-                  label: `${city.name}, ${city.countryCode}`,
-                };
-              }),
-            };
-          });
-      };
-    
-
-    return (
-        <div>
-
-        </div>
+  const loadOptions = (inputValue: SearchDataProps) => {
+    return fetch(
+      `https://api.openweathermap.org/data/2.5/cities?minPopulation=1000000&namePrefix=${inputValue}`
     )
-}
+      .then((response) => response.json())
+      .then((response) => {
+        return {
+          options: response.data.map((city: SearchDataProps) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name}, ${city.countryCode}`,
+            };
+          }),
+        };
+      });
+  };
+
+  const handleOnChange = (searchData: SearchDataProps) => {
+    if (setSearch) {
+      setSearch(searchData);
+    }
+
+    if (onSearchChange) {
+      onSearchChange(searchData);
+    }
+  };
+
+  return (
+    <AsyncPaginate
+      placeholder="Search for city"
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+    />
+  );
+};
+export default Search;
--- src/types/component-types.ts
+++ src/types/component-types.ts
@@ -1,12 +1,13 @@
 export interface SearchDataProps {
-  value: string;
-  label: string;
-  onSearchChange: string;
-  inputValue: string;
-  city: string;
-  latitude: string;
-  longitude: string;
-  countryCode: string;
-  name: string;
-
-}
+    value: string;
+    label: string;
+    onSearchChange?: (searchData: SearchDataProps) => void;
+    inputValue: string;
+    city: string;
+    latitude: string;
+    longitude: string;
+    countryCode: string;
+    name: string;
+    searchData: string;
+  }
+  
