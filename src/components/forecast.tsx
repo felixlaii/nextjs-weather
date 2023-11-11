8ca1845a577630e0077fce8dde46387fcaa9e6b4
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-11-10 19:13:13 -0500

    added accessible accordion

--- src/components/forecast.tsx
+++ src/components/forecast.tsx
@@ -1,3 +1,12 @@
+import React from "react";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+} from "react-accessible-accordion";
+
 const Forecast: React.FC = () => {
     return (
         <div></div>
