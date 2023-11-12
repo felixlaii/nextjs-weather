c02e7d46b3a53f8419990bec16c6fd24e4470129
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-11-12 16:21:39 -0500

    api function for weather data

---
+++ src/pages/api/weather-api.ts
@@ -0,0 +1,21 @@
+import { WeatherData } from "@/components/current-weather";
+import { ForecastDataItem } from "@/components/forecast";
+import { SearchDataProps } from "@/types/component-types";
+
+const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/weather";
+const FORECAST_API_URL = "https://api.openweathermap.org/data/2.5/forecast";
+
+export const getWeatherData = async (lat:string, lon: string) => {
+    try {
+        const response = await fetch(
+            `${WEATHER_API_URL}?lat=${lat}&lon=${lon}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&units=metric`
+        );
+        const data: WeatherData = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error fetching current weather data:", error);
+        throw new Error(
+            "Error fetching current weather data. Please try again."
+        )
+    }
+}
