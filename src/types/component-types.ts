a776153a2d0839eb4f90ce9fccaec1b32deb7ea3
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-11-12 14:25:04 -0500

    cleaned up code

--- src/types/component-types.ts
+++ src/types/component-types.ts
@@ -1,18 +1,15 @@
 export interface SearchDataProps {
-    value: string;
-    label: string;
-    inputValue: string;
-    city: string;
-    latitude: string;
-    longitude: string;
-    countryCode: string;
-    name: string;
-    searchData: string;
-  }
+  value: string;
+  label: string;
+  inputValue: string;
+  city: string;
+  latitude: string;
+  longitude: string;
+  countryCode: string;
+  name: string;
+  searchData: string;
+}
 
-
-  
-  // The onSearchChange prop should have this signature:
-  export interface SearchProps {
-    onSearchChange: (searchData: SearchDataProps) => void;
-  }
+export interface SearchProps {
+  onSearchChange: (searchData: SearchDataProps) => void;
+}
