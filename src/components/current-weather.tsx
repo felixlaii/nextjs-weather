a0562e14c7c4154a34dc448e96a0f7590af674f6
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-11-14 12:40:22 -0500

    cleaned code

--- src/components/current-weather.tsx
+++ src/components/current-weather.tsx
@@ -2,82 +2,83 @@ import React from "react";
 import Image from "next/image";
 
 export interface WeatherData {
-    city: string;
-    weather: {
-      description: string;
-      icon: string;
-    }[];
-    main: {
-      temp: number;
-      feels_like: number;
-      humidity: number;
-      pressure: number;
-    };
-    wind: {
-      speed: number;
-    };
-  }
-  
-  interface CurrentWeatherProps {
-    data: WeatherData;
-  }
-  
-  const parameterRow = "flex justify-between";
-  const parameterLabel = "text-left font-400 text-[12px]"
-  const parameterValue = "text-right font-600 text-[12px]"
-  const weather = "w-1/3 border-6 rounded-lg shadow-xl text-white bg-gray-600 mx-auto mt-20 mb-0 p-0 sm:p-4"
-  const top = "flex justify-between items-center"
-  const city = "font-bold text-18 leading-1 m-0 tracking-wider"
-  const temperature = "font-bold text-70 tracking-wide mb-10"
-  const parameterLabelTop = "border-b border-white text-left font-400 text-[12px]"
+  city: string;
+  weather: {
+    description: string;
+    icon: string;
+  }[];
+  main: {
+    temp: number;
+    feels_like: number;
+    humidity: number;
+    pressure: number;
+  };
+  wind: {
+    speed: number;
+  };
+}
 
-  const CurrentWeather: React.FC<CurrentWeatherProps> = ({ data }) => {
-    if (!data || !data.city || !data.weather || !data.weather[0]) {
-      return <div>Error: Invalid data</div>;
-    }
-    return (
-      <div className={weather}>
-        <div className={top}>
-          <div>
-            <p className={city}>{data.city}</p>
-            <p className="text-md">{data.weather[0].description}</p>
-          </div>
-          <Image
-            alt="weather"
-            width={100}
-            height={100}
-            className="w-[100px]"
-            src={`/icons/${data.weather[0].icon}.png`}
-          />
+interface CurrentWeatherProps {
+  data: WeatherData;
+}
+
+const parameterRow = "flex justify-between";
+const parameterValue = "text-right font-600 text-[12px]";
+const weather =
+  "w-1/3 border-6 rounded-lg shadow-xl text-white bg-gray-600 mx-auto mt-20 mb-0 p-0 sm:p-4";
+const top = "flex justify-between items-center";
+const city = "font-bold text-18 leading-1 m-0 tracking-wider";
+const temperature = "font-bold text-70 tracking-wide mb-10";
+const parameterLabelTop =
+  "border-b border-white text-left font-400 text-[12px]";
+
+const CurrentWeather: React.FC<CurrentWeatherProps> = ({ data }) => {
+  if (!data || !data.city || !data.weather || !data.weather[0]) {
+    return <div>Error: Invalid data</div>;
+  }
+  return (
+    <div className={weather}>
+      <div className={top}>
+        <div>
+          <p className={city}>{data.city}</p>
+          <p className="text-md">{data.weather[0].description}</p>
         </div>
-        <div className={top}>
-          <p className={temperature}>{Math.round(data.main.temp)}°C</p>
-          <div className="w-full pl-20">
-            <div className={parameterRow}>
-              <span className="text-left">Details</span>
-            </div>
-            <div className={parameterRow}>
-              <span className="text-left text-[12px]">Feels like</span>
-              <span className="text-right text-[12px]">
-                {Math.round(data.main.feels_like)}°C
-              </span>
-            </div>
-            <div className={parameterRow}>
-              <span className={parameterLabelTop}>Wind</span>
-              <span className={parameterValue}>{data.wind.speed} m/s</span>
-            </div>
-            <div className={parameterRow}>
-              <span className={parameterLabelTop}>Humidity</span>
-              <span className={parameterValue}>{data.main.humidity}%</span>
-            </div>
-            <div className={parameterRow}>
-              <span className={parameterLabelTop}>Pressure</span>
-              <span className={parameterValue}>{data.main.pressure} hPa</span>
-            </div>
+        <Image
+          alt="weather"
+          width={100}
+          height={100}
+          className="w-[100px]"
+          src={`/icons/${data.weather[0].icon}.png`}
+        />
+      </div>
+      <div className={top}>
+        <p className={temperature}>{Math.round(data.main.temp)}°C</p>
+        <div className="w-full pl-20">
+          <div className={parameterRow}>
+            <span className="text-left">Details</span>
+          </div>
+          <div className={parameterRow}>
+            <span className="text-left text-[12px]">Feels like</span>
+            <span className="text-right text-[12px]">
+              {Math.round(data.main.feels_like)}°C
+            </span>
+          </div>
+          <div className={parameterRow}>
+            <span className={parameterLabelTop}>Wind</span>
+            <span className={parameterValue}>{data.wind.speed} m/s</span>
+          </div>
+          <div className={parameterRow}>
+            <span className={parameterLabelTop}>Humidity</span>
+            <span className={parameterValue}>{data.main.humidity}%</span>
+          </div>
+          <div className={parameterRow}>
+            <span className={parameterLabelTop}>Pressure</span>
+            <span className={parameterValue}>{data.main.pressure} hPa</span>
           </div>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default CurrentWeather;
--- src/components/forecast.tsx
+++ src/components/forecast.tsx
@@ -2,129 +2,133 @@ import React from "react";
 import {
   Accordion,
   AccordionItem,
-  AccordionItemHeading,
   AccordionItemButton,
   AccordionItemPanel,
 } from "react-accessible-accordion";
 import Image from "next/image";
 
 const WEEK_DAYS = [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-  ];
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
 
-
-  export interface ForecastDataItem {
-    city: string;
-    weather: {
-      icon: string;
-      description: string;
-    }[];
-    main: {
-      temp_max: number;
-      temp_min: number;
-      pressure: number;
-      humidity: number;
-      sea_level: number;
-      feels_like: number;
-    };
-    wind: {
-      speed: number;
-    };
-    clouds: {
-      all: number;
-    };
-  }
-  
-  export interface ForecastProps {
-    data: {
-      list: ForecastDataItem[];
-    };
-  }
-
-
-  const Forecast: React.FC<ForecastProps> = ({ data }) => {
-    const dayInAWeek = new Date().getDay();
-    const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(
-      WEEK_DAYS.slice(0, dayInAWeek)
-    );
-  
-    const current = new Date();
-    let currentDate = current.toString().slice(0, 15);
-  
-    // Ensure that data and data.list are not null or undefined
-    const forecastData = data?.list?.slice(0, 7) || [];
-  
-    const title = "text-[23px] font-700"
-    const dailyItem = "bg-gray-200 rounded-xl h-10 m-1 items-center cursor-pointer flex text-base px-5"
-    const iconSmall = "w-[40px]"
-    const dailyItemDay = "cursor-pointer text-gray-700 flex-1 font-semibold ml-4"
-    const weatherDescription = "cursor-pointer flex-1 mr-4 text-right"
-    const dailyDetailsGrid = "grid gap-x-4 grid-cols-2 py-1 px-4"
-    const dailyDetailsGridItem = "items-center flex h-8 justify-between"
-    return (
-      <>
-        <label className={title}>Date: {currentDate}</label>
-        <Accordion allowZeroExpanded>
-          {forecastData.map((item, idx) => (
-            <AccordionItem key={idx}>
-              <AccordionItemButton>
-                <div className={dailyItem}>
-                  <Image
-                    src={{ src: `/icons/${item.weather[0]?.icon}.png`, width: 100, height: 100 }}
-                    className={iconSmall}
-                    alt={item.weather[0]?.description || "Weather icon"}
-                  />
-  
-                  <label className={dailyItemDay}>{forecastDays[idx]}</label>
-                  <label className={weatherDescription}>
-                    {item.weather[0]?.description ??
-                      "No description available"}
-                  </label>
-                  <label className="text-gray-600">
-                    {Math.round(item.main.temp_max)}°C /
-                    {Math.round(item.main.temp_min)}°C
-                  </label>
-                </div>
-              </AccordionItemButton>
-              <AccordionItemPanel>
-                <div className={dailyDetailsGrid}>
-                <div className={dailyDetailsGridItem}>                    
-                <label className="text-gray-600">Pressure:</label>
-                    <label className="text-gray-800">{item.main.pressure}</label>
-                  </div>
-                  <div className={dailyDetailsGridItem}>
-                    <label className="text-gray-600">Humidity:</label>
-                    <label className="text-gray-800">{item.main.humidity}</label>
-                  </div>
-                  <div className={dailyDetailsGridItem}>
-                    <label className="text-gray-600">Clouds:</label>
-                    <label className="text-gray-800">{item.clouds.all}%</label>
-                  </div>
-                  <div className={dailyDetailsGridItem}>
-                    <label className="text-gray-600">Wind speed:</label>
-                    <label className="text-gray-800">{item.wind.speed} m/s</label>
-                  </div>
-                  <div className={dailyDetailsGridItem}>
-                    <label className="text-gray-600">Sea level:</label>
-                    <label className="text-gray-800">{item.main.sea_level}m</label>
-                  </div>
-                  <div className={dailyDetailsGridItem}>
-                    <label className="text-gray-600">Feels like:</label>
-                    <label className="text-gray-800">{item.main.feels_like}°C</label>
-                  </div>
-                </div>
-              </AccordionItemPanel>
-            </AccordionItem>
-          ))}
-        </Accordion>
-      </>
-    );
+export interface ForecastDataItem {
+  city: string;
+  weather: {
+    icon: string;
+    description: string;
+  }[];
+  main: {
+    temp_max: number;
+    temp_min: number;
+    pressure: number;
+    humidity: number;
+    sea_level: number;
+    feels_like: number;
   };
-  
-  export default Forecast;
+  wind: {
+    speed: number;
+  };
+  clouds: {
+    all: number;
+  };
+}
+
+export interface ForecastProps {
+  data: {
+    list: ForecastDataItem[];
+  };
+}
+
+const Forecast: React.FC<ForecastProps> = ({ data }) => {
+  const dayInAWeek = new Date().getDay();
+  const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(
+    WEEK_DAYS.slice(0, dayInAWeek)
+  );
+
+  const current = new Date();
+  let currentDate = current.toString().slice(0, 15);
+
+  const forecastData = data?.list?.slice(0, 7) || [];
+
+  const title = "text-[23px] font-700";
+  const dailyItem =
+    "bg-gray-200 rounded-xl h-10 m-1 items-center cursor-pointer flex text-base px-5";
+  const iconSmall = "w-[40px]";
+  const dailyItemDay = "cursor-pointer text-gray-700 flex-1 font-semibold ml-4";
+  const weatherDescription = "cursor-pointer flex-1 mr-4 text-right";
+  const dailyDetailsGrid = "grid gap-x-4 grid-cols-2 py-1 px-4";
+  const dailyDetailsGridItem = "items-center flex h-8 justify-between";
+  return (
+    <>
+      <label className={title}>Date: {currentDate}</label>
+      <Accordion allowZeroExpanded>
+        {forecastData.map((item, idx) => (
+          <AccordionItem key={idx}>
+            <AccordionItemButton>
+              <div className={dailyItem}>
+                <Image
+                  src={{
+                    src: `/icons/${item.weather[0]?.icon}.png`,
+                    width: 100,
+                    height: 100,
+                  }}
+                  className={iconSmall}
+                  alt={item.weather[0]?.description || "Weather icon"}
+                />
+
+                <label className={dailyItemDay}>{forecastDays[idx]}</label>
+                <label className={weatherDescription}>
+                  {item.weather[0]?.description ?? "No description available"}
+                </label>
+                <label className="text-gray-600">
+                  {Math.round(item.main.temp_max)}°C /
+                  {Math.round(item.main.temp_min)}°C
+                </label>
+              </div>
+            </AccordionItemButton>
+            <AccordionItemPanel>
+              <div className={dailyDetailsGrid}>
+                <div className={dailyDetailsGridItem}>
+                  <label className="text-gray-600">Pressure:</label>
+                  <label className="text-gray-800">{item.main.pressure}</label>
+                </div>
+                <div className={dailyDetailsGridItem}>
+                  <label className="text-gray-600">Humidity:</label>
+                  <label className="text-gray-800">{item.main.humidity}</label>
+                </div>
+                <div className={dailyDetailsGridItem}>
+                  <label className="text-gray-600">Clouds:</label>
+                  <label className="text-gray-800">{item.clouds.all}%</label>
+                </div>
+                <div className={dailyDetailsGridItem}>
+                  <label className="text-gray-600">Wind speed:</label>
+                  <label className="text-gray-800">{item.wind.speed} m/s</label>
+                </div>
+                <div className={dailyDetailsGridItem}>
+                  <label className="text-gray-600">Sea level:</label>
+                  <label className="text-gray-800">
+                    {item.main.sea_level}m
+                  </label>
+                </div>
+                <div className={dailyDetailsGridItem}>
+                  <label className="text-gray-600">Feels like:</label>
+                  <label className="text-gray-800">
+                    {item.main.feels_like}°C
+                  </label>
+                </div>
+              </div>
+            </AccordionItemPanel>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </>
+  );
+};
+
+export default Forecast;
