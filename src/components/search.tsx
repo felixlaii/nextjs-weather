d642e4cf4a57b0bf8b1d6004d420395d3074a065
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-11-25 13:30:14 -0500

    deleted Async

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,79 +1,93 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { SearchDataProps } from "@/types/component-types";
-import { AsyncPaginate } from "react-select-async-paginate";
-import { ActionMeta, SingleValue } from "react-select";
-import { SearchProps } from "@/types/component-types";
 
-const Search: React.FC<SearchProps> = ({ onSearchChange }) => {
-  const [search, setSearch] = useState<null | SearchDataProps>(null);
+const Search: React.FC<{
+  onSearchChange: (data: SearchDataProps | null) => void;
+}> = ({ onSearchChange }) => {
+  const [inputValue, setInputValue] = useState("");
+  const [options, setOptions] = useState<SearchDataProps[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [selectedOption, setSelectedOption] = useState<SearchDataProps | null>(
+    null
+  );
 
-  const loadOptions = (inputValue: string) => {
-    return fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
-    )
-      .then((response) => response.json())
-      .then((response) => {
-        return {
-          options: [
-            {
-              value: `${response.coord.lat} ${response.coord.lon}`,
-              label: `${response.name}, ${response.sys.country}`,
-              inputValue: "",
-              city: response.name,
-              latitude: String(response.coord.lat),
-              longitude: String(response.coord.lon),
-              countryCode: response.sys.country,
-              name: response.name,
-              searchData: "",
-            },
-          ],
-        };
-      });
-  };
+  const loadOptions = async (inputValue: string) => {
+    setIsLoading(true);
 
-  const handleOnChange = (
-    selectedOption: SingleValue<SearchDataProps>,
-    actionMeta: ActionMeta<SearchDataProps>
-  ) => {
-    const searchData: SearchDataProps | null = selectedOption
-      ? {
-          value: selectedOption.value,
-          label: selectedOption.label,
-          inputValue: "",
-          city: "",
-          latitude: "",
-          longitude: "",
-          countryCode: "",
-          name: "",
-          searchData: "",
-        }
-      : {
-          value: "",
-          label: "",
-          inputValue: "",
-          city: "",
-          latitude: "",
-          longitude: "",
-          countryCode: "",
-          name: "",
-          searchData: "",
-        };
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
+      );
 
-    setSearch(searchData);
+      if (response.ok) {
+        const data = await response.json();
+        const newOptions: SearchDataProps[] = [
+          {
+            value: `${data.coord.lat} ${data.coord.lon}`,
+            label: `${data.name}, ${data.sys.country}`,
+            inputValue: "",
+            city: data.name,
+            latitude: String(data.coord.lat),
+            longitude: String(data.coord.lon),
+            countryCode: data.sys.country,
+            name: data.name,
+            searchData: "",
+          },
+        ];
 
-    if (onSearchChange) {
-      onSearchChange(searchData);
+        setOptions(newOptions);
+      } else {
+        setOptions([]);
+      }
+    } catch (error) {
+      console.error("Error fetching options:", error);
+      setOptions([]);
+    } finally {
+      setIsLoading(false);
     }
   };
 
+  const handleInputChange = (value: string) => {
+    setInputValue(value);
+    loadOptions(value);
+  };
+
+  const handleOnChange = (selectedOption: SearchDataProps | null) => {
+    setSelectedOption(selectedOption);
+
+    if (onSearchChange) {
+      onSearchChange(selectedOption);
+    }
+  };
+
+  useEffect(() => {
+    if (!inputValue) {
+      setOptions([]);
+    }
+  }, [inputValue]);
+
   return (
-    <AsyncPaginate
-      placeholder="Search for city"
-      debounceTimeout={600}
-      value={search}
-      onChange={handleOnChange}
-      loadOptions={loadOptions}
-    />
+    <div>
+      <input
+        className="w-full px-1 pt-2 pb-2 rounded-lg"
+        type="text"
+        placeholder="Search for city"
+        value={inputValue}
+        onChange={(e) => handleInputChange(e.target.value)}
+      />
+      {isLoading && <p>Loading...</p>}
+      <ul>
+        {options.map((option) => (
+          <li
+            className="px-1"
+            key={option.value}
+            onClick={() => handleOnChange(option)}
+          >
+            {option.label}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
