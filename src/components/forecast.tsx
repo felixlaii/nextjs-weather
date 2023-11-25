a42d7b48a97da924d8e9f7740252260367ae08df
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-11-25 13:16:09 -0500

    deleted code

--- src/components/forecast.tsx
+++ src/components/forecast.tsx
@@ -6,6 +6,7 @@ import {
   AccordionItemPanel,
 } from "react-accessible-accordion";
 import Image from "next/image";
+import { ForecastProps } from "@/types/component-types";
 
 const WEEK_DAYS = [
   "Monday",
@@ -16,34 +17,6 @@ const WEEK_DAYS = [
   "Saturday",
   "Sunday",
 ];
-
-export interface ForecastDataItem {
-  city: string;
-  weather: {
-    icon: string;
-    description: string;
-  }[];
-  main: {
-    temp_max: number;
-    temp_min: number;
-    pressure: number;
-    humidity: number;
-    sea_level: number;
-    feels_like: number;
-  };
-  wind: {
-    speed: number;
-  };
-  clouds: {
-    all: number;
-  };
-}
-
-export interface ForecastProps {
-  data: {
-    list: ForecastDataItem[];
-  };
-}
 
 const Forecast: React.FC<ForecastProps> = ({ data }) => {
   const dayInAWeek = new Date().getDay();
@@ -56,34 +29,28 @@ const Forecast: React.FC<ForecastProps> = ({ data }) => {
 
   const forecastData = data?.list?.slice(0, 7) || [];
 
-  const title = "text-[23px] font-700";
-  const dailyItem =
-    "bg-gray-200 rounded-xl h-10 m-1 items-center cursor-pointer flex text-base px-5";
-  const iconSmall = "w-[40px]";
-  const dailyItemDay = "cursor-pointer text-gray-700 flex-1 font-semibold ml-4";
-  const weatherDescription = "cursor-pointer flex-1 mr-4 text-right";
-  const dailyDetailsGrid = "grid gap-x-4 grid-cols-2 py-1 px-4";
-  const dailyDetailsGridItem = "items-center flex h-8 justify-between";
   return (
     <>
-      <label className={title}>Date: {currentDate}</label>
+      <label className="text-[23px] font-700">Date: {currentDate}</label>
       <Accordion allowZeroExpanded>
         {forecastData.map((item, idx) => (
           <AccordionItem key={idx}>
             <AccordionItemButton>
-              <div className={dailyItem}>
+              <div className="bg-gray-200 rounded-xl h-10 m-1 items-center cursor-pointer flex text-base px-5">
                 <Image
                   src={{
                     src: `/icons/${item.weather[0]?.icon}.png`,
                     width: 100,
                     height: 100,
                   }}
-                  className={iconSmall}
+                  className="w-[40px]"
                   alt={item.weather[0]?.description || "Weather icon"}
                 />
 
-                <label className={dailyItemDay}>{forecastDays[idx]}</label>
-                <label className={weatherDescription}>
+                <label className="cursor-pointer text-gray-700 flex-1 font-semibold ml-4">
+                  {forecastDays[idx]}
+                </label>
+                <label className="cursor-pointer flex-1 mr-4 text-right">
                   {item.weather[0]?.description ?? "No description available"}
                 </label>
                 <label className="text-gray-600">
@@ -93,30 +60,30 @@ const Forecast: React.FC<ForecastProps> = ({ data }) => {
               </div>
             </AccordionItemButton>
             <AccordionItemPanel>
-              <div className={dailyDetailsGrid}>
-                <div className={dailyDetailsGridItem}>
+              <div className="grid gap-x-4 grid-cols-2 py-1 px-4">
+                <div className="items-center flex h-8 justify-between">
                   <label className="text-gray-600">Pressure:</label>
                   <label className="text-gray-800">{item.main.pressure}</label>
                 </div>
-                <div className={dailyDetailsGridItem}>
+                <div className="items-center flex h-8 justify-between">
                   <label className="text-gray-600">Humidity:</label>
                   <label className="text-gray-800">{item.main.humidity}</label>
                 </div>
-                <div className={dailyDetailsGridItem}>
+                <div className="items-center flex h-8 justify-between">
                   <label className="text-gray-600">Clouds:</label>
                   <label className="text-gray-800">{item.clouds.all}%</label>
                 </div>
-                <div className={dailyDetailsGridItem}>
+                <div className="items-center flex h-8 justify-between">
                   <label className="text-gray-600">Wind speed:</label>
                   <label className="text-gray-800">{item.wind.speed} m/s</label>
                 </div>
-                <div className={dailyDetailsGridItem}>
+                <div className="items-center flex h-8 justify-between">
                   <label className="text-gray-600">Sea level:</label>
                   <label className="text-gray-800">
                     {item.main.sea_level}m
                   </label>
                 </div>
-                <div className={dailyDetailsGridItem}>
+                <div className="items-center flex h-8 justify-between">
                   <label className="text-gray-600">Feels like:</label>
                   <label className="text-gray-800">
                     {item.main.feels_like}°C
