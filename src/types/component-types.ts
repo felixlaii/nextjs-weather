9b8a92cdd93f02d4a2127446fb9fabb8d9a84b4e
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-11-25 13:13:00 -0500

    moved interface

--- src/types/component-types.ts
+++ src/types/component-types.ts
@@ -13,3 +13,24 @@ export interface SearchDataProps {
 export interface SearchProps {
   onSearchChange: (searchData: SearchDataProps) => void;
 }
+
+export interface WeatherData {
+  city: string;
+  weather: {
+    description: string;
+    icon: string;
+  }[];
+  main: {
+    temp: number;
+    feels_like: number;
+    humidity: number;
+    pressure: number;
+  };
+  wind: {
+    speed: number;
+  };
+}
+
+export interface CurrentWeatherProps {
+  data: WeatherData;
+}
